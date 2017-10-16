369156dff7c293f1c924996bb06a885c67608bbc
Rich Mama <postmaster.richmama@gmail.com>
2017-10-16 18:48:55 +0100

    Moved public files to fundsforlife directory

--- public/js/alerts.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-function showNotification(style, title, message, position, timeout, type){
-$('body').pgNotification({style:style,title:title,message:message,position:position,timeout:0,type:type}).show();
-}
