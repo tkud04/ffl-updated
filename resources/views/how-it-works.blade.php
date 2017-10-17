0314c90d6e6bba577be199311ac561ef8fef210c
Rich Mama <postmaster.richmama@gmail.com>
2017-10-17 12:18:26 +0100

    How it works upload

---
+++ resources/views/how-it-works.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('title',"How It Works")
+
+@section('content')
+
+    <section id="testimonial">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">How It Works</h2>
+                <p class="text-center wow fadeInDown">FundsForLife makes it easy to enjoy what matters most in your life!</p>
+            </div>
+
+            <div class="row">
+                 <div class="col-sm-12">
+                      <h2 class="text-primary">Registration</h2></p><br><br>
+                      <p><span style="text-align: center; ">To register on FundsForLife you have to purchase an activation pin. You will use this pin to activate your account.</span></p><br>
+                      <p><span style="text-align: center; ">You can purchase your pins from our accredited vendors by clicking <a href="{{url('vendors')}}">here</a> or by joining our social media help groups.</span></p><br>
+                      <p><span style="text-align: center; ">After purchasing your pin, you will be taken through our 5-step registration process, after which your account will be registered and activated. </span></p><br>
+                      <p><span style="text-align: center; color: #f00; ">Ensure you provide correct details as our team of experts constantly check our servers for fraudulent information and remove such information accordingly.</span></p><br><br>
+                      
+                      <h2 class="text-primary">Providing Help</h2><br><br>
+                      <p><span style="text-align: center; ">To enjoy donations from other members of our community you must donate to the participant we provide you with. </span></p><br>
+                      <p><span style="text-align: center; ">If participants to donate to are available, you will be merged INSTANTLY. Otherwise it may take between 15 minutes to 24 hours for you to be merged to donate.</span></p><br>
+                      <p><span style="text-align: center; ">Once merged, the banking and contact details of the participant will be provided to you in your dashboard. Make sure you contact the participant to let him or her know you are about to donate.</span></p><br>
+                      <p><span style="text-align: center; color: #f00;">After donation, the recipient will confirm or reject your donation. The deadline for making payment to a matched participant is 5 hours, after which your account will be BLOCKED. </span></p><br><br>
+                      	
+                      <h2 class="text-primary">Getting Help</h2><br><br>
+                      <p><span style="text-align: center; ">After providing help, you become eligible to receive donations from 2 participants. </span></p><br>
+                      <p><span style="text-align: center; ">You wait till you are matched, and then once you have received payment you confirm the donation. </span></p><br>
+                      <p><span style="text-align: center; color: #f00;">If you don't confirm the donation within 5 hours after receiving payment, the system will automatically confirm the donation. </span></p><br>
+                      <p><span style="text-align: center;">Once you've received help from one participant, the second donation will be LOCKED. To unlock you must provide help to another participant that the system will match you with. This is to ensure the continuity of our website and to curb PH defaulters and fraudulent individuals. </span></p><br>
+                      <p><span style="text-align: center; ">If participants to donate to are available, you will be merged INSTANTLY. Otherwise it may take between 15 minutes to 24 hours for you to be merged to donate. </span></p><br>
+                      <p><span style="text-align: center; ">Once merged, the banking and contact details of the participant will be provided to you in your dashboard. Make sure you contact the participant to let him or her know you are about to donate.</span></p><br>
+                      <p><span style="text-align: center; color: #f00;">After donation, the recipient will confirm or reject your donation. The deadline for making payment to a matched participant is 5 hours, after which your account will be BLOCKED. </span></p><br>
+                      <p><span style="text-align: center; ">Once you have provided help to the participant, your second payment will be UNLOCKED. </span></p><br>
+                      <p><span style="text-align: center; ">You wait till you are matched, and then once you have received payment you confirm the donation. </span></p><br>
+                      <p><span style="text-align: center; ">After confirming donation, if your activation pin is still valid you begin the receiving cycle all over again. </span></p><br>        
+                      <p><span style="text-align: center; color: #f00; ">NOTE: your activation pin is valid for 5 receiving cycles, after which you must purchase another activation pin to continue enjoying donations. </span></p><br><br>          
+                      
+
+                 </div>
+            </div>
+
+        </div>
+    </section> <!--/#testimonial-->
+
+@stop
