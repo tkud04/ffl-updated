4776a3f32c304961db09b9f3ed4a316127a6e3e3
Rich Mama <postmaster.richmama@gmail.com>
2017-10-17 10:22:07 +0100

    Hiw

---
+++ resources/views/h-i-w.blade.php
@@ -0,0 +1,57 @@
+<br>
+<section id="how-it-works" >
+        <div class="container">
+
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">How It Works</h2>
+            </div>
+
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <h3><strong>PART A: REGISTRATION</strong></h3>
+                        <i class="fa fa-check"></i> You register & verify your Email<br>
+                        <i class="fa fa-check"></i> You provide Activation Pin<br>
+                        <i class="fa fa-check"></i> You input & verify your Phone Number<br>
+                        <i class="fa fa-check"></i> You provide your Bank Details & select Plan<br>
+                        <br><br>
+                        </div>
+                        	
+                        
+                        <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <h3><strong>PART B: DONATION</strong></h3>
+                        <i class="fa fa-check"></i> You wait..<br>
+                        <i class="fa fa-check"></i> You are assigned ONE participant to donate to.<br>
+                        <i class="fa fa-check"></i> You make donation<br>
+                        <i class="fa fa-check"></i> Participant confirms your donation.<br>
+                        <br><br>
+                        </div>
+
+                        <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <h3><strong>PART C: RECEIVING</strong></h3>
+                        <i class="fa fa-check"></i> You wait..<br>
+                        <i class="fa fa-check"></i> You are assigned ONE participant to receive from.<br>
+                        <i class="fa fa-check"></i> You receive donation<br>
+                        <i class="fa fa-check"></i> You confirm or reject donation.<br>
+                        <i class="fa fa-check"></i>Second donation will be LOCKED.<br>
+                        <i class="fa fa-check"></i> You wait..<br>
+                        <i class="fa fa-check"></i> You are assigned ONE participant to donate to.<br>
+                        <i class="fa fa-check"></i> You make donation<br>
+                        <i class="fa fa-check"></i> Participant confirms your donation.<br>
+                        <i class="fa fa-check"></i>Second donation will be UNLOCKED.<br>
+                        <i class="fa fa-check"></i> You are assigned ONE participants to receive from.<br>
+                        <i class="fa fa-check"></i> You receive donation<br>
+                        <i class="fa fa-check"></i> You confirm or reject donation.<br>
+                        <i class="fa fa-check"></i> If your activation pin is still valid, you start the cycle all over again.<br>
+                        <br><br>                    	
+                    </div><!--/.col-md-12-->          
+                </div>
+            </div><!--/.row-->    
+            <div class="row">
+            	<div class="col-md-12 col-sm-12 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+            	 <center> <a class="btn btn-success" href="{{url('how-it-works')}}">Learn more</a></center>
+                </div>
+           </div>            
+        </div><!--/.container-->
+    </section><!--/#services-->
+   <br>
