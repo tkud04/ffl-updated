504cfcfa6a15480b058ceec406a4b25f9efdc034
Rich Mama <postmaster.richmama@gmail.com>
2017-10-18 01:06:03 +0100

    Updated emails.verify

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,5 +1,6 @@
 <h3>Verify Your FundsForLife Account</h3>
 <center>
   <p>Hi! You recently registered {{$email}} for FundsForLife, please click the link below to verify your email:<br></p>
-  <a href="{{url('verify/$id')}}"></a>
+  <a href="{{url('verify/$id')}}">Click here to verify your Email address</a><br>
+  Or you can copy and paste the following link to your browser: {{url('verify/$id')}}
 </center>
