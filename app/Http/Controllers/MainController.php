b694502aa06144f5106ab0f59f48faa1081c690c
Rich Mama <postmaster.richmama@gmail.com>
2017-10-18 02:49:14 +0100

    More typo corrections in post r2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -411,7 +411,7 @@ class MainController extends Controller {
               $status = $this->helpers->useActivationPin($user, $pin);
               $s = "PH"; 
           	if($user->role == "special") $s = "GH"; 
-      	    $this->setUserStatus($user, $status);
+      	    $this->helpers->setUserStatus($user, $status);
 
               Session::flash("r2-status", $status);
               $u = "r2";
